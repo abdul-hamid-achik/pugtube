05b33699224087ac7e09a4c21d9236031f4ba062
abdul hamid <abdulachik@icloud.com>
2023-02-15 02:30:05 -0600

    Saving transcoder

--- src/pages/api/inngest.ts
+++ src/pages/api/inngest.ts
@@ -7,11 +7,18 @@
 import { inngest } from '../../server/background'
 import { serve } from "inngest/next"
 import { prisma } from "../../server/db"
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
+import { S3 } from 'aws-sdk';
+import { createFFmpeg, fetchFile, FS } from '@ffmpeg/ffmpeg'
+import fs from 'fs'
+
+const s3 = new S3({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 const transcode = inngest.createFunction("Transcode to HLS", "hls.transcode", async ({ event }) => {
     const { uploadId } = event.data as { uploadId: string };
-    // @ts-ignore
     const upload = await prisma.upload.findUnique({
         where: {
             id: uploadId
@@ -19,7 +26,7 @@ const transcode = inngest.createFunction("Transcode to HLS", "hls.transcode", as
     });
 
     if (!upload) {
-        throw new Error("Upload not found")
+        throw new Error("Upload not found");
     }
 
     // Load FFmpeg
@@ -27,18 +34,32 @@ const transcode = inngest.createFunction("Transcode to HLS", "hls.transcode", as
     await ffmpeg.load();
 
     // Download the file from S3 and write it to the virtual file system
-    ffmpeg.FS('writeFile', 'input.mp4', await fetchFile(`https://pugtube-dev.s3.amazonaws.com/${upload.id}`));
+    const inputFilePath = `/tmp/${upload.id}`;
+    await s3.getObject({ Bucket: process.env.AWS_S3_BUCKET as string, Key: upload.id })
+        .createReadStream()
+        .pipe(fs.createWriteStream(inputFilePath))
+
+    ffmpeg.FS('writeFile', inputFilePath, await fetchFile(`file://${inputFilePath}`));
 
     // Transcode the video to HLS format
-    await ffmpeg.run('-i', 'input.mp4', '-codec', 'copy', '-start_number', '0', '-hls_time', '10', '-hls_list_size', '0', '-f', 'hls', 'output.m3u8');
+    await ffmpeg.run('-i', inputFilePath, '-codec', 'copy', '-start_number', '0', '-hls_time', '10', '-hls_list_size', '0', '-f', 'hls', '/tmp/output.m3u8');
 
-    // Get the transcoded file from the virtual file system and save it to S3
-    const hlsOutput = ffmpeg.FS('readFile', 'output.m3u8');
-    // TODO: Save the HLS output to S3
-    console.log(hlsOutput)
+    // Get the transcoded files from the virtual file system and upload them to S3
+    const outputDirPath = '/tmp/output';
+    FS.mkdir(outputDirPath);
+    const hlsOutput = ffmpeg.FS('readdir', outputDirPath);
+    for (const outputFileName of hlsOutput) {
+        const outputFilePath = `${outputDirPath}/${outputFileName}`;
+        const outputData = ffmpeg.FS('readFile', outputFilePath);
+        const key = `${upload.id}/${outputFileName}`;
+        await s3.putObject({ Bucket: process.env.AWS_S3_BUCKET as string, Key: key, Body: outputData }).promise();
+    }
+
+
 
     console.log("Transcoding video", uploadId);
 });
+
 
 
 export default serve("pugtube", [transcode])
