0ce2a835f3ea8f05e33e1965c15b992bc2725de3
abdul hamid <abdulachik@icloud.com>
2023-02-25 22:38:35 -0600

    collecting analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/function-component-definition */
 import { api } from '@/utils/api';
+import { Analytics } from '@vercel/analytics/react';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { type AppType } from 'next/app';
-
 export { reportWebVitals } from 'next-axiom';
 
 import '@/styles/globals.css';
@@ -12,10 +12,12 @@ import '@/styles/globals.css';
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
-}) => (
+}) => (<>
   <SessionProvider session={session}>
     <Component {...pageProps} />
   </SessionProvider>
+  <Analytics />
+</>
 );
 
 export default api.withTRPC(MyApp);
