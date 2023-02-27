7f8ef9ff2a8a7fb2ebd03c2be4a716ad1d3deb8e
abdul hamid <abdulachik@icloud.com>
2023-02-26 21:00:22 -0600

    Aiming to load all files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "cypress";
 import dotenv from "dotenv";
+import fs from "fs";
 
-dotenv.config();
+console.log("Loading .env file (if it exists)...");
+if (fs.existsSync(".env")) {
+  console.log("loading .env file")
+  dotenv.config();
+} else if (fs.existsSync(".env.local")) {
+  console.log("loading .env.local file")
+  dotenv.config({ path: ".env.local" });
+} else if (fs.existsSync(".env.development.local")) {
+  console.log("loading .env.development.local file")
+  dotenv.config({ path: ".env.development.local" });
+} else if (fs.existsSync(".env.production.local")) {
+  console.log("loading .env.production.local file")
+  dotenv.config({ path: ".env.production.local" });
+} else if (fs.existsSync(".env.preview.local")) {
+  console.log("loading .env.preview.local file")
+  dotenv.config({ path: ".env.preview.local" });
+}
+
 
 export default defineConfig({
   projectId: "jcfv2t",
