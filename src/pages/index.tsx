0546d179dad9aef489aa51b65afd3cc1e8bfc18c
abdul hamid <abdulachik@icloud.com>
2023-03-13 18:58:35 -0600

    improving styles by using a layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Header from '@/components/header';
+import Layout from '@/components/layout';
 import Spinner from '@/components/spinner';
 import VideoCard from '@/components/video-card';
+import { NextPageWithLayout } from '@/pages/_app';
 import { api } from '@/utils/api';
 import { GetServerSidePropsContext } from 'next';
-import Head from 'next/head';
+import { ReactElement } from 'react';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
@@ -11,7 +12,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   }
 }
 
-export default function Home() {
+export const Page: NextPageWithLayout = () => {
   const { data, isError, isLoading } = api.video.getAll.useQuery({
     page: 1,
     perPage: 9,
@@ -19,36 +20,21 @@ export default function Home() {
 
 
   return (
-    <>
-      <Head>
-        <title>Pugtube</title>
-        <meta name="description" content="A free video sharing service" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="h-full bg-gray-900 py-2">
-        <div className="sticky top-0 z-10 bg-gray-900 pl-4">
-          <Header />
-        </div>
-        <div className="container relative mx-auto max-w-screen-xl px-4">
-          <section className="my-8">
-            {/* hero section */}
-            <div className="flex flex-col gap-4">
-              <h1 className="text-4xl font-bold text-white">Pugtube</h1>
-              <p className="text-gray-300">A free video sharing service</p>
-            </div>
-          </section>
-          <section className="my-8 min-h-screen">
-            {/* recommended videos section */}
-            <div className="flex h-full w-full flex-col gap-4 overflow-y-auto md:grid md:grid-cols-2 lg:grid-cols-3">
-              {isLoading && <Spinner />}
-              {!isLoading && !isError && data?.map(({ video, author }) => (<VideoCard key={video.id} video={video} author={author} />))}
-            </div>
-          </section>
-          <section className="my-8">
-            {/* subscribed channels section */}
-          </section>
-        </div>
+    <section className="my-8 min-h-screen">
+      <div className="flex h-full w-full flex-col gap-4 overflow-y-auto md:grid md:grid-cols-2 lg:grid-cols-3">
+        {isLoading && <Spinner />}
+        {!isLoading && !isError && data?.map(({ video, author }) => (<VideoCard key={video.id} video={video} author={author} />))}
       </div>
-    </>
+    </section>
   );
 }
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}
+
+export default Page;
