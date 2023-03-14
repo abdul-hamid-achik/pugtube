d2a4390c2a593301fc6c294277d8e094a46a04ff
abdul hamid <abdulachik@icloud.com>
2023-03-13 23:47:01 -0600

    adding infinite query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import VideoCard from '@/components/video-card';
 import { NextPageWithLayout } from '@/pages/_app';
 import { api } from '@/utils/api';
 import { GetServerSidePropsContext } from 'next';
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useRef, useState } from 'react';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
@@ -13,32 +13,70 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 }
 
 export const Page: NextPageWithLayout = () => {
-  const { data, error, isError, isLoading } = api.video.getAll.useQuery({
-    page: 1,
-    perPage: 9,
-  })
+  const [page, setPage] = useState(0);
+  const { data, error, isError, isLoading, fetchNextPage } = api.video.feed.useInfiniteQuery({
+    limit: 10,
+  }, {
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+    // initialCursor: 1
+  });
+  const loaderRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    if (!isLoading) return;
+
+    let current = loaderRef.current;
+    const options = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver((entries = []) => {
+      if (entries.length > 0 && entries[0]!.isIntersecting) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    }, options);
+
+    if (current) {
+      observer.observe(current);
+    }
+
+    return () => {
+      if (current) {
+        observer.unobserve(current);
+      }
+    };
+  }, [loaderRef, isLoading]);
+
+  useEffect(() => {
+    fetchNextPage();
+  }, [page, fetchNextPage]);
 
   return (
     <section className="">
-      {isError && <div className="bg-red-400 text-white"><div className="p-4">
-        <p>{error?.message}</p>
-      </div>
-      </div>}
+      {isError && (
+        <div className="bg-red-400 text-white">
+          <div className="p-4">
+            <p>{error?.message}</p>
+          </div>
+        </div>
+      )}
       {isLoading && <Spinner />}
       <div className="w-full flex-col items-center justify-center gap-4 overflow-y-auto md:grid md:grid-cols-2 lg:grid-cols-3">
-        {!isLoading && !isError && data?.map(({ video, author }) => (<VideoCard key={video.id} video={video} author={author} />))}
+        {!isLoading &&
+          !isError &&
+          data?.pages[page]?.items.map(({ video, author }) => (
+            <VideoCard key={video.id} video={video} author={author} />
+          ))}
+        <div ref={loaderRef}></div>
       </div>
     </section>
   );
-}
+};
 
 Page.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <Layout>
-      {page}
-    </Layout>
-  )
-}
+  return <Layout>{page}</Layout>;
+};
 
 export default Page;
