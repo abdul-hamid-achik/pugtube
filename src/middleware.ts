a51c4b586e287500a57ecbbae4b94ba73c915dbc
abdul hamid <abdulachik@icloud.com>
2023-03-13 22:30:48 -0600

    reomving middleware to debug css issue in prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { getAuth, withClerkMiddleware } from '@clerk/nextjs/server'
+import { withClerkMiddleware } from '@clerk/nextjs/server'
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
@@ -6,27 +6,27 @@ import { NextResponse } from 'next/server'
 const publicPaths = ['/', '/sign-in*', '/sign-up*', '/watch*', '/terms-of-service', '/privacy-policy', '/api*', '/channel*']
 
 const isPublic = (path: string) => {
-
     return publicPaths.find(x =>
         path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
     )
 }
 
 export default withClerkMiddleware((request: NextRequest) => {
-    if (isPublic(request.nextUrl.pathname)) {
-        return NextResponse.next()
-    }
-    // if the user is not signed in redirect them to the sign in page.
-    const { userId } = getAuth(request)
-
-    if (!userId) {
-        // redirect the users to /pages/sign-in/[[...index]].ts
-
-        const signInUrl = new URL('/sign-in', request.url)
-        signInUrl.searchParams.set('redirect_url', request.url)
-        return NextResponse.redirect(signInUrl)
-    }
     return NextResponse.next()
+    // if (isPublic(request.nextUrl.pathname)) {
+    //     return NextResponse.next()
+    // }
+    // // if the user is not signed in redirect them to the sign in page.
+    // const { userId } = getAuth(request)
+
+    // if (!userId) {
+    //     // redirect the users to /pages/sign-in/[[...index]].ts
+
+    //     const signInUrl = new URL('/sign-in', request.url)
+    //     signInUrl.searchParams.set('redirect_url', request.url)
+    //     return NextResponse.redirect(signInUrl)
+    // }
+    // return NextResponse.next()
 })
 
 export const config = { matcher: '/((?!_next/image|_next/static|favicon.ico).*)' };
