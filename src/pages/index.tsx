39696096f9df3320a6e586bcdcf1ec3ef382ef55
abdul hamid <abdulachik@icloud.com>
2023-03-15 20:16:41 -0600

    loading first a set of results and fixing infinite scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,56 @@ import Layout from '@/components/layout';
 import Spinner from '@/components/spinner';
 import VideoCard from '@/components/video-card';
 import { NextPageWithLayout } from '@/pages/_app';
+import { prisma } from '@/server/db';
 import { api } from '@/utils/api';
+import { getSignedUrl } from '@/utils/s3';
+import { clerkClient } from '@clerk/nextjs/server';
+import { Video } from '@prisma/client';
 import { GetServerSidePropsContext } from 'next';
 import { ReactElement } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
+  let videos = await prisma.video.findMany({
+    take: 9,
+    skip: 0,
+    cursor: undefined,
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  videos = await Promise.all(videos.map(async (video: Video) => ({ ...video, thumbnailUrl: video.thumbnailUrl ? await getSignedUrl(video.thumbnailUrl as string) : null })));
+
+  const authors = await clerkClient.users.getUserList({
+    userId: videos.map((video) => video.userId),
+  });
+
+  const items = videos.map((video) => ({
+    video,
+    author: authors.find((author) => author.id === video.userId)!,
+  }));
+
   return {
-    props: {}
-  }
+    props: {
+      initialData: {
+        items: JSON.parse(JSON.stringify(items)),
+        nextCursor: null,
+      },
+    },
+  };
 }
 
-export const Page: NextPageWithLayout = () => {
-  const { data, error, isError, isLoading, fetchNextPage } = api.video.feed.useInfiniteQuery({
-    limit: 10,
-  }, {
-    getNextPageParam: (lastPage) => lastPage.nextCursor,
-    // initialCursor: 1
-  });
+export const Page: NextPageWithLayout<{ initialData: any }> = ({ initialData }) => {
+  const { data, error, isError, isLoading, fetchNextPage } = api.video.feed.useInfiniteQuery(
+    {
+      limit: 9,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+      initialData: { pages: [initialData], pageParams: [undefined] },
+    }
+  );
 
   const hasNextPage = data?.pages.some((page) => !!page.nextCursor) || false;
 
