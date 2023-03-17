719410eb1860ef06ccfca684e984568b0fd235c2
abdul hamid <abdulachik@icloud.com>
2023-03-17 01:35:05 -0600

    making this work with next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,29 @@
 // @ts-check
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds.
- */
+
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
 import { withSentryConfig } from "@sentry/nextjs";
 import { withAxiom } from "next-axiom";
+import path from "path";
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  /* If trying out the experimental appDir, comment the i18n config out
-   * @see https://github.com/vercel/next.js/issues/41980 */
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
   },
-  webpack(config) {
+  webpack(config, { isServer }) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
+    if (isServer) {
+      config.resolve.alias["@ffmpeg/ffmpeg/src/createFFmpeg"] = path.resolve(
+        path.dirname(import.meta.url),
+        "./src/utils/ffmpeg.ts"
+      );
+    }
     return config;
   },
   sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
     hideSourceMaps: true,
   },
   images: {
@@ -63,8 +59,3 @@ const config = {
 export default withSentryConfig(withAxiom(config), {
   silent: true,
 });
-
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
