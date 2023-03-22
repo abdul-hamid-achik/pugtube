2db5d1665dfea84bae59c3138c3888296a555496
abdul hamid <abdulachik@icloud.com>
2023-03-22 03:43:26 -0600

    hopefully this fixes uploads

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -15,6 +15,7 @@ import { type AppRouter } from '@/server/api/root';
 
 const getBaseUrl = () => {
   if (typeof window !== 'undefined') return ''; // browser should use relative url
+  if (process.env.NODE_ENV === 'production') return 'https://pugtube.dev'; // prod SSR should use relative url (same domain
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
