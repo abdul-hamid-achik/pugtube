6de296b99917a8c8e6b0eda21ba116dcb6e02ccb
abdul hamid <abdulachik@icloud.com>
2023-03-28 11:51:33 -0600

    Typo in tests.yml

--- src/server/jobs/generate-preview.ts
+++ src/server/jobs/generate-preview.ts
@@ -12,61 +12,61 @@ export default async function generateThumbnail({
   fileName: string;
 }) {
   let ffmpeg;
-  // try {
-  ffmpeg = await createFFmpeg();
-  log.info(`Generating Gif for upload ID: ${uploadId}...`);
-  const upload = await getObject({
-    Bucket: process.env.AWS_S3_BUCKET,
-    Key: `originals/${uploadId}/${fileName}`,
-  });
+  try {
+    ffmpeg = await createFFmpeg();
+    log.info(`Generating Gif for upload ID: ${uploadId}...`);
+    const upload = await getObject({
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: `originals/${uploadId}/${fileName}`,
+    });
 
-  const inputFileName = fileName;
+    const inputFileName = fileName;
 
-  const buffer = await streamToBuffer(upload!.Body as Readable);
-  const gifOutputFileName = `${uploadId}.gif`;
-  await ffmpeg.FS("writeFile", inputFileName, new Uint8Array(buffer));
-  await ffmpeg.run(
-    "-i",
-    inputFileName,
-    "-ss",
-    "00:00:00",
-    "-t",
-    "3",
-    "-vf",
-    "fps=10,scale=w=min(720\\,iw):h=min(480\\,ih):flags=lanczos",
-    "-c:v",
-    "gif",
-    `${gifOutputFileName}`
-  );
+    const buffer = await streamToBuffer(upload!.Body as Readable);
+    const gifOutputFileName = `${uploadId}.gif`;
+    await ffmpeg.FS("writeFile", inputFileName, new Uint8Array(buffer));
+    await ffmpeg.run(
+      "-i",
+      inputFileName,
+      "-ss",
+      "00:00:00",
+      "-t",
+      "3",
+      "-vf",
+      "fps=10,scale=w=min(720\\,iw):h=min(480\\,ih):flags=lanczos",
+      "-c:v",
+      "gif",
+      `${gifOutputFileName}`
+    );
 
-  log.info(`preview generated for upload ID: ${uploadId}`);
+    log.info(`preview generated for upload ID: ${uploadId}`);
 
-  const gif = await ffmpeg.FS("readFile", gifOutputFileName);
-  await putObject({
-    Bucket: process.env.AWS_S3_BUCKET,
-    Key: `previews/${uploadId}.gif`,
-    Body: gif,
-    ContentType: "image/gif",
-    ContentLength: gif.length,
-  });
+    const gif = await ffmpeg.FS("readFile", gifOutputFileName);
+    await putObject({
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: `previews/${uploadId}.gif`,
+      Body: gif,
+      ContentType: "image/gif",
+      ContentLength: gif.length,
+    });
 
-  const previewUrl = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/previews/${uploadId}.gif`;
+    const previewUrl = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/previews/${uploadId}.gif`;
 
-  await prisma.video.update({
-    where: {
-      uploadId,
-    },
-    data: {
-      previewUrl,
-    },
-  });
+    await prisma.video.update({
+      where: {
+        uploadId,
+      },
+      data: {
+        previewUrl,
+      },
+    });
 
-  await ffmpeg.FS("unlink", inputFileName);
-  await ffmpeg.FS("unlink", gifOutputFileName);
+    await ffmpeg.FS("unlink", inputFileName);
+    await ffmpeg.FS("unlink", gifOutputFileName);
 
-  log.info(`Updated video with preview URL: ${previewUrl}`);
-  // } catch (err: any) {
-  //   ffmpeg?.exit();
-  //   log.error(err);
-  // }
+    log.info(`Updated video with preview URL: ${previewUrl}`);
+  } catch (err: any) {
+    ffmpeg?.exit();
+    log.error(err);
+  }
 }
