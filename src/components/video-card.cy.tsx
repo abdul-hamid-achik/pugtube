9ec18de2f8af7c773a09406218699ae819293ff8
abdul hamid <abdulachik@icloud.com>
2023-03-28 11:44:07 -0600

    Fixing basic tests

--- src/components/video-card.cy.tsx
+++ src/components/video-card.cy.tsx
@@ -1,58 +1,59 @@
 import { faker } from "@faker-js/faker";
 import VideoCard from "./video-card";
+import { ClerkProvider } from "@clerk/nextjs";
+import MockRouter from "@cypress/utils/mock-router";
 
 const author = {
-    id: faker.random.alphaNumeric(),
-    username: faker.internet.userName(),
-    phoneNumbers: [],
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    profileImageUrl: faker.internet.avatar(),
-    emailAddresses: [],
-    passwordEnabled: false,
-    totpEnabled: false,
-    backupCodeEnabled: false,
-    twoFactorEnabled: false,
-    banned: false,
-    createdAt: Date.now(),
-    updatedAt: Date.now(),
-    gender: "test",
-    primaryEmailAddressId: "test",
-    primaryPhoneNumberId: "test",
-    birthday: faker.date.past().toISOString(),
-    primaryWeb3WalletId: "test",
-    externalId: "test",
-    unsafeMetadata: {},
-    externalAccounts: [],
-    web3Wallets: [],
-    lastSignInAt: Date.now(),
-    publicMetadata: {},
-    privateMetadata: {},
-}
+  id: faker.random.alphaNumeric(),
+  username: faker.internet.userName(),
+  phoneNumbers: [],
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  profileImageUrl: faker.internet.avatar(),
+  emailAddresses: [],
+  passwordEnabled: false,
+  totpEnabled: false,
+  backupCodeEnabled: false,
+  twoFactorEnabled: false,
+  banned: false,
+  createdAt: Date.now(),
+  updatedAt: Date.now(),
+  gender: "test",
+  primaryEmailAddressId: "test",
+  primaryPhoneNumberId: "test",
+  birthday: faker.date.past().toISOString(),
+  primaryWeb3WalletId: "test",
+  externalId: "test",
+  unsafeMetadata: {},
+  externalAccounts: [],
+  web3Wallets: [],
+  lastSignInAt: Date.now(),
+  publicMetadata: {},
+  privateMetadata: {},
+};
 
 const video = {
-    id: faker.random.alphaNumeric(),
-    title: faker.lorem.sentence(),
-    description: faker.lorem.paragraph(),
-    duration: parseFloat(faker.random.numeric()),
-    thumbnailUrl: faker.image.imageUrl(),
-    category: faker.random.word(),
-    createdAt: new Date(),
-    userId: faker.random.alphaNumeric(),
-    uploadId: faker.random.alphaNumeric(),
-    hlsPlaylistId: faker.random.alphaNumeric(),
-}
+  id: faker.random.alphaNumeric(),
+  title: faker.lorem.sentence(),
+  description: faker.lorem.paragraph(),
+  duration: parseFloat(faker.random.numeric()),
+  thumbnailUrl: faker.image.imageUrl(),
+  category: faker.random.word(),
+  createdAt: new Date(),
+  userId: faker.random.alphaNumeric(),
+  uploadId: faker.random.alphaNumeric(),
+  hlsPlaylistId: faker.random.alphaNumeric(),
+  previewUrl: "",
+};
 
-describe(
-    "VideoCard",
-    () => {
-        it(
-            "should render correctly",
-            () => {
-                cy.mount(<VideoCard video={video}
-                    author={author}
-                />);
-            },
-        );
-    },
-)
+describe("VideoCard", () => {
+  it("should render correctly", () => {
+    cy.mount(
+      <MockRouter>
+        <ClerkProvider>
+          <VideoCard video={video} author={author} />
+        </ClerkProvider>
+      </MockRouter>
+    );
+  });
+});
