0094a55fd5abbc6aa25ff6525595830123cbc721
abdul hamid <abdulachik@icloud.com>
2023-04-08 14:54:08 -0600

    adding quick frontend for monitoring background jobs

--- src/pages/monitor/index.tsx
+++ src/pages/monitor/index.tsx
@@ -1,47 +1,82 @@
 import { api } from "@/utils/api";
 import Timestamp from "@/components/timestamp";
+import { JobType } from "bullmq";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
+const states: JobType[] = [
+  "completed",
+  "failed",
+  "active",
+  "delayed",
+  "waiting",
+  "waiting-children",
+  "paused",
+  "repeat",
+  "wait",
+];
 function Monitor() {
-  const { data, refetch } = api.background.jobsByState.useQuery([
-    "completed",
-    "failed",
-    "active",
-    "delayed",
-    "waiting",
-    "waiting-children",
-    "paused",
-    "repeat",
-    "wait",
-  ]);
+  const [state, setState] = useState<JobType | null>();
+  const { data, refetch } = api.background.jobsByState.useQuery(
+    state ? [state] : states
+  );
 
   const { mutateAsync: remove } =
     api.background.removeJobFromQueue.useMutation();
+
   return (
-    <>
-      <h1>Monitor</h1>
-      {data?.map((job) => (
-        <div key={job.id}>
-          <p>{job.id}</p>
-          <h2>{job.name}</h2>
-          <p>{JSON.stringify(job.data, null, 2)}</p>
-          <p>{JSON.stringify(job.progress, null, 2)}</p>
-          <p>{job.failedReason}</p>
-          <p>{job.returnvalue}</p>
-          <p>{job.stacktrace}</p>
-          <Timestamp timestamp={job.timestamp} />
-          <div>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-white">Monitor</h1>
+      <ul className="flex w-full flex-row text-white">
+        {states.map((state) => (
+          <li key={state} className="p-2">
             <button
+              className={`px-4 py-2 text-white ${
+                state === state ? "bg-blue-500" : "bg-gray-500"
+              }`}
               onClick={() => {
-                remove(job.id!).then(() => refetch());
+                if (state === state) {
+                  setState(null);
+                } else {
+                  setState(state);
+                }
               }}
-              type="button"
             >
-              Delete
+              {state}
             </button>
-          </div>
-        </div>
-      ))}
-    </>
+          </li>
+        ))}
+      </ul>
+      <ul className="bg-gray-600 p-4 text-white">
+        {data?.map((job) => (
+          <li
+            key={job.id}
+            className="border-b-2 border-dashed border-gray-50 px-0 py-4"
+          >
+            <div className="flex w-full justify-between">
+              <p>ID: {job.id}</p>
+              <button
+                onClick={() => {
+                  remove(job.id!).then(() => refetch());
+                }}
+                className="bg-red-500 px-4 py-2 text-white"
+                type="button"
+              >
+                <XMarkIcon className="h-4 w-4" />
+                <span className="sr-only">Remove job from queue</span>
+              </button>
+            </div>
+            <h2>Name: {job.name}</h2>
+            <p>Data: {JSON.stringify(job.data, null, 2)}</p>
+            <p>Progress: {JSON.stringify(job.progress, null, 2)}</p>
+            <p>Failure Reason: {job.failedReason}</p>
+            <p>Return Value: {job.returnvalue}</p>
+            <p>Strack Trace: {job.stacktrace}</p>
+            <Timestamp timestamp={job.timestamp} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
